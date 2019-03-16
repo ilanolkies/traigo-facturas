aa9fc77a945d07afa8b2e2e5c97234ddc1d33207
Ilan Olkies <ilanolkies@outlook.com>
2019-03-16 03:12:58 -0300

    Test events

--- test/1_traigo_facutras.js
+++ test/1_traigo_facutras.js
@@ -31,6 +31,13 @@ contract('TraigoFacturas', async (accounts) => {
     assert.equal(emptyEntry[0], 0);
   });
 
+  it('should emit new entry event', async () => {
+    const tx = await traigoFacturas.newEntry();
+
+    const event = tx.logs[0].event;
+    assert.equal(event, 'NewEntry');
+  });
+
   it('should input and close one entry', async () => {
     await traigoFacturas.newEntry();
 
@@ -41,5 +48,15 @@ contract('TraigoFacturas', async (accounts) => {
 
     const index = await traigoFacturas.index();
     assert.equal(index, 1);
+  });
+
+  it('should emit close entry event', async () => {
+    await traigoFacturas.newEntry();
+    const tx = await traigoFacturas.closeEntry(0);
+
+    const event = tx.logs[0].event;
+    const index = tx.logs[0].args.index;
+    assert.equal(event, 'CloseEntry');
+    assert.equal(index, 0);
   });
 });
